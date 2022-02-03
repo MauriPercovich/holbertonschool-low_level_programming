ba7ac64d4b567547985270c21d18e237fbf71614
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-03 09:37:46 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print
+ * nothing
+ * Return: Always (0)
+**/
+int main(void)
+{
+Console.WriteLine("sizeof(int): {0}", sizeof(int));
+            Console.WriteLine("sizeof(float): {0}", sizeof(float));
+	                Console.WriteLine("sizeof(char): {0}", sizeof(char));
+			            Console.WriteLine("sizeof(double): {0}", sizeof(double));
+				                Console.WriteLine("sizeof(bool): {0}", sizeof(bool));
+}
