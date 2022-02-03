c73b80217d7a3fca36ce5c79b2e095812006847c
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-03 09:44:47 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,14 @@ int main(void)
 {
 int intType;
 float floatType;
-double doubleType;
+long longType;
+long long longlongType;
 char charType;
 
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of long: %zu bytes\n", sizeof(longType));
+printf("Size of long long: %zu bytes\n", sizeof(longlongType));
 printf("Size of char: %zu byte\n", sizeof(charType));
 
 return 0;
