fb153ca7c76637416eb3012a2ed9742ac96b5487
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-03 09:14:31 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - print
- *
- * Return: Always 0
- **/
-int main(void)	
-{	
-int printf(const char "with proper grammar, but the outcome is a piece of art,");
+ * comment
+ * Return: (0)
+**/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
