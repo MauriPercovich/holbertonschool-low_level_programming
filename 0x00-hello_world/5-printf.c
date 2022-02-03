fa80e0afcfe526ce0711a9ba2300760d536b35d2
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-03 09:09:38 -0800

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print
+ *
+ * Return: Always 0
+ **/
+int main(void)	
+{	
+int printf(const char "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+{
+int gcc(-wall);
+	return (0);
+
