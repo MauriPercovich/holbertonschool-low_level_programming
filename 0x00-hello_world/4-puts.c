0298a92432e3f7bd6f944e4c3724618e379615e8
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-03 08:50:52 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,11 @@
  * a
  *comment
 **/
-int main (void)
+int main(void)
+/**
+ * task
+**/
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
