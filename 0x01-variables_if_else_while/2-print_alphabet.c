f9abc8242487ca87f6ef0a4948dc4b17d0fdd6b8
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-04 07:11:03 -0800

    tasks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,11 @@
  * Return: 0
  **/
 
-int main (void)
+int main(void)
 {
 	char x = "a";
-	for(char x = 'a'; x <= 'z'; x++)
+
+	for (char x = 'a'; x <= 'z'; x++)
 	putchar(x);
 	putchar ('\n');
 	return (0);
