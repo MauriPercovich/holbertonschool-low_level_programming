7e3333cb2aa8baae42739f5f958d3fd2e9fa0cd4
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-04 09:03:57 -0800

    task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print
+*
+* Return: 0
+**/
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+
+if (ch != 'e')
+if (ch != 'q')
+
+putchar(ch);
+putchar ('\n');
+return (0);
+}
