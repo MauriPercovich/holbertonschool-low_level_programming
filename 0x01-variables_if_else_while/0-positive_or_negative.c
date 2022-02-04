f3bf525cb6e213ff18b5c905a38317837f0cd7d4
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-04 07:06:53 -0800

    tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - printing
  *
@@ -11,12 +12,11 @@ int main(void)
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-					if(n > 0){
-					int puts
-					("is positive")}
-					else {
-					int puts("is negative")}
-					if(n = 0) {
-					int puts("is zero")		
+				if (n < 0)
+				{	printf("%d is negative\n", n);	}
+				else if (n == 0)
+				 {       printf("%d is zero\n", n);  }
+				else
+				{	printf("%d is positive\n", n);	}
 					return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print
+ *
+ * Return: 0
+ **/
+
+int main (void)
+{
+	char x = "a";
+	for(char x = 'a'; x <= 'z'; x++)
+	putchar(x);
+	putchar ('\n');
+	return (0);
+}
