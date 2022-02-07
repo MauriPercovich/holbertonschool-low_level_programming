efa1b2653349f3a591e3a32fc21ef03176df453d
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-07 08:54:50 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: always 0
+**/
+
+void print_alphabet(void)
+{
+char x = 'a';
+
+while (x <= 'z')
+
+
+{
+_putchar(x);
+	x++;
+}
+
+_putchar('\n');
+
+}
