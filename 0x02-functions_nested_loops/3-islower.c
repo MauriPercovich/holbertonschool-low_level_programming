90f07c870fac96a373f9cb9ee597192ca06fd275
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-07 09:48:09 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _islower - print
+ *
+ * Return: always 0
+**/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+
+{
+return (1);
+}
+
+else
+
+{
+return (0);
+}
+
+
+
+}
