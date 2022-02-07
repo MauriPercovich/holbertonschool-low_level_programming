f3eee364f5f88687565bfe2a3dfab44e509fd0f7
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-07 13:14:08 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *@i: variable
+ *Return: always 0
+**/
+
+int print_last_digit(int i)
+{
+int x = i % 10;
+
+if (i < 0)
+
+x = x * -1;
+
+_putchar(x);
+
+return (x);
+}
+
