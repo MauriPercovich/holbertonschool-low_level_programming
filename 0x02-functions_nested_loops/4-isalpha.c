16e8b8f9a38b06198010ba47802e9fae6252440b
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-07 10:59:25 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,13 +8,13 @@
 int _isalpha(int c)
 {
 
-if (('a' <= c <= 'z') || ('A' <= c <= 'Z'))
+if (('a'<= c && c <= 'z') || ('A' <= c && c <= 'Z'))
 
-return ('1');
+return (1);
 
 else
 
-return ('0');
+return (0);
 
 
 }
