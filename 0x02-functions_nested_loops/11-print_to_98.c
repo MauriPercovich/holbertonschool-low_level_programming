07fdb66bc3bc5f3994dde9b481653755361efe24
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-07 16:36:48 -0800

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - function thar prints numbers to 98
+ * @x: number
+ * Return: always 0
+**/
+void print_to_98(int n)
+{
+while (n <= 97)
+{
+	if (n != 98)
+	printf("%d, ", n);
+
+
+	else
+
+	printf("%d", n);
+
+	n++;
+
+}
+
+while (n > 97)
+{
+	if (n != 98)
+	printf("%d, ", n);
+
+
+	else
+
+	printf("%d", n);
+
+	n--;
+
+}
+
+	printf("\n");
+
+}
