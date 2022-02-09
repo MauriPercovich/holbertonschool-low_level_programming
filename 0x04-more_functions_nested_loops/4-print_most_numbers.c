2986e927d96cc63ec5e1f073a0506b6807b8bfde
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-09 09:31:14 -0800

    task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ *
+ * Retrun: always 0
+**/
+void print_most_numbers(void)
+{
+int x;
+
+for (x = '0' ; x <= '9' ; x++)
+{
+if (x != '2' && x != '4')
+
+{
+_putchar(x);
+}
+}
+_putchar('\n');
+}
+
