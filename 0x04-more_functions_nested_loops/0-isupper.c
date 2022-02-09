2ae6ead6a61175eaea37e5c388cd4305382b185f
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-09 07:40:07 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - checks uppercase
+ * @c: letter
+ * Return: always 0
+**/
+int _isupper(int c)
+{
+
+if ('A' <= c && c <= 'Z')
+
+return (1);
+
+
+else
+
+
+return (0);
+
+
+
+}
