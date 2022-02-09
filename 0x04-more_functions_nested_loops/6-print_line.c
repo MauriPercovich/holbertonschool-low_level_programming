2f03dff284b298c3a19fabfd641098b384ff2f52
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-09 10:18:19 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints line with n length
+ * @n: length
+ * Return: always 0
+**/
+void print_line(int n)
+{
+
+int x;
+
+x = 0;
+	while (x > n)
+{
+	_putchar('_');
+
+}
+
+	_putchar ('\n');
+}
