2e1b36f483da5e973013683728f574aa9d683141
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-21 10:28:24 -0800

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - a function that locates a substring
+ *@haystack: string
+ *@needle: string
+ * Return: always 0.
+**/
+char *_strstr(char *haystack, char *needle)
+{
+int y;
+
+        for (; *haystack != '\0'; haystack++)
+        {
+                for (y = 0; needle[y] != '\0'; y++)
+                {
+                if (*haystack == *needle)
+                {
+                        return (haystack);
+                }
+        }
+        if (*haystack == *needle)
+        {
+                return (haystack);
+        }
+        }
+return (0);
+
+
+
+
+}
