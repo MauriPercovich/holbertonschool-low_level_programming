4be70c190066a92ccb8bc2d9375c4d6949460d2e
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-23 07:29:40 -0800

    task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - factorial
+ * @n: number
+ * return: always o
+**/
+int factorial(int n)
+{
+
+if (n == 0)
+{
+	return (1);
+}
+else if (n > 0)
+{
+	return (n * factorial(n - 1));
+}
+else 
+{
+	return (-1);
+}
+}
