c883fe8da2694b5d1c95104a4cd4187e415b9eb5
MauriPercovich <mauriciopercovich22@gmail.com>
2022-02-23 04:37:15 -0800

    task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: always 0
+**/
+void _puts_recursion(char *s)
+{
+int x;
+
+_putchar(s[x]);
+x++;
+if (s[x] != '\0') 
+{
+_putchar(s[x]);
+}
+
+
+int main(void)
+{
+_putchar(s[x]);
+return (0);
+
+}
