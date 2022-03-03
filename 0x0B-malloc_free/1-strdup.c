91b3dd979b1be1f849d32ccbfe563106eeddf64e
MauriPercovich <mauriciopercovich22@gmail.com>
2022-03-02 18:59:10 -0800

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,33 +11,33 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  char *array;
-  int i = 0;
-  int j = 0;
-  int size = 0;
+char *array;
+int i = 0;
+int j = 0;
+int size = 0;
 
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-  while (s1[i] != '\0')
-    i++;
-  while (s2[j] != '\0')
-    j++;
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
 
-  size = i + j + 1;
+size = i + j + 1;
 
-  array = malloc(size * sizeof(char));
+array = malloc(size * sizeof(char));
 
-  if (array == NULL)
-    return (NULL);
+if (array == NULL)
+return (NULL);
 
-  for (i = 0; s1[i] != '\0'; i++)
-    array[i] = s1[i];
+for (i = 0; s1[i] != '\0'; i++)
+array[i] = s1[i];
 
-  for (j = 0; s2[j] != '\0'; j++)
-    array[i + j] = s2[j];
+for (j = 0; s2[j] != '\0'; j++)
+array[i + j] = s2[j];
 
-  return (array);
+return (array);
 }
