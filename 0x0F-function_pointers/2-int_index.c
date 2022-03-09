c5c73aafb950b78a3018dfdfad2f25e2b2a8dc47
MauriPercovich <mauriciopercovich22@gmail.com>
2022-03-09 08:17:33 -0800

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,11 +4,16 @@
  * @array: array
  * @size: size
  * @cmp: pointer to function
+ * Return: always 0
 **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int x;
 
+if (array == NULL || cmp == NULL)
+{
+	return (-1);
+}
 if (size <= 0)
 {
 	return (-1);
@@ -16,12 +21,12 @@ if (size <= 0)
 
 for (x = 0; x < size; x++)
 {
-	if (cmp(array[x]) == 1)
+	if ((cmp)(array[x]) != 0)
 	{
-		return(x);
+		return (x);
 	}
-	
+
 }
 
-return (x);
+return (-1);
 }
