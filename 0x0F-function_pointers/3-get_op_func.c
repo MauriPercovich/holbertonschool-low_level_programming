cde816a79fbae92a40f29daf0701e55d2608fb23
MauriPercovich <mauriciopercovich22@gmail.com>
2022-03-09 11:03:56 -0800

    task

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+/**
+ *@get_op_func - func
+ *@s: s
+ *
+ * Return: always 0
+**/
+int (*get_op_func(char *s))(int, int);
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
+int i;
+
+while (i < 5)
+{
+	if (ops[i] == s)
+	{
+		return (ops[i].f);
+	}
+	i++;
+}
+return (NULL);
+}
+
+#endif
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ *@a: int
+ *@b: int
+ *@op_add: add
+ *Return: always 0
+**/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ *@a: int
+ *@b: int
+ *@op_sub: sub
+ *Return: always 0
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *@a: int
+ *@b: int
+ *@op_mul: mul
+ *Return: always 0
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *@a: int
+ *@b: int
+ *@op_div: div
+ *Return: always 0
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *@a: int
+ *@b: int
+ *@op_mod: mod
+ *Return: always 0
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
